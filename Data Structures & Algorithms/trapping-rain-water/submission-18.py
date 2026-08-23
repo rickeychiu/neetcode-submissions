4c9db366e73402c2bb819011cc709dc4b43ee931
Rickey Chiu <86750833+rickeychiu@users.noreply.github.com>
2026-08-23 16:43:29 -0700

    Add: trapping-rain-water - submission-18

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-18.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        
+        # prefix: leftToRight
+        leftToRight = [None] * len(height)
+        leftToRight[0] = height[0]
+        for i in range(1, len(height)):
+            leftToRight[i] = max(leftToRight[i-1], height[i])
+            print(leftToRight[i])
+        
+        # suffix: rightToLeft
+        rightToLeft = [None] * len(height)
+        rightToLeft[-1] = height[-1]
+        for j in range(len(height)-2, -1, -1):
+            rightToLeft[j] = max(rightToLeft[j+1], height[j])
+            #print(rightToLeft[j])
+        # stored water = min(prefix[i], suffix[i])
+        water = [None] * len(height)
+        for k in range(len(height)):
+            water[k] = min(leftToRight[k], rightToLeft[k]) - height[k]
+            
+            
+        
+        return sum(water)
