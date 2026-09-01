135d8a8bfc3fbe7cc638a3b11746aea54447f321
Rickey Chiu <86750833+rickeychiu@users.noreply.github.com>
2026-08-31 20:49:37 -0700

    Add: course-schedule - submission-3

---
+++ Data Structures & Algorithms/course-schedule/submission-3.py
@@ -0,0 +1,38 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+    
+        adjList = {} # store as { node1 : node2, node3 }
+        # node 1 must be taken before node2 and node3
+        for prereq, course in prerequisites:
+            if prereq not in adjList:
+                adjList[prereq] = []
+            adjList[prereq].append(course)
+        
+        # now check if it's valid by checking if there are no cycles
+        states = {} # store as { node : state }
+        # state (None) = unvisited, = 1 means exploring, = 2 means visited
+        def hasCycle(node):
+            if node in states:
+                if states[node] == 1:
+                    return True  # cycle, current exploration sees same node
+                if states[node] == 2:
+                    return False # already checked a previous call
+            
+            states[node] = 1 # mark current unvisited node as exploring
+            for nb in adjList.get(node, []):
+                if hasCycle(nb):
+                    return True
+            
+            # now, done checking. set everything to 2 for next run
+            states[node] = 2
+            return False
+        
+        # run hasCycle from every node
+    
+        for node, _ in adjList.items():
+            if hasCycle(node):
+                return False
+        
+        return True
+
+        
